0abeb43e63fd2ada3fc01bcca275278520bce37b
Youssef Algendy <ymgendy2000@gmail.com>
2021-10-30 23:59:48 +0200

    Refined UserLogin endpoint

--- src/Routes/user.route.js
+++ src/Routes/user.route.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require('../Models/user.model');
+const User = require("../Models/user.model");
 
-router.get("/Login", async(req,res)=>
-{
-    const userEmail = req.body.email;
-    const userPass = req.body.password;
-    const user = await User.find({email:userEmail});
-    if (user!=null)
-    {
-        if(userPass===user.password)
-            {
-                const token = user.generateAuthToken();
-                user.authTokens.push(token);
-                res.header("x-user-auth-token",token);
-                res.status(200).send();
-            }
-        else
-            res.send("Error");
-    }
-    else 
-        res.send("Error");
+router.post("/Login", async (req, res) => {
+  const userEmail = req.body.email;
+  const userPass = req.body.password;
+  const user = await User.findOne({ email: userEmail });
+
+  if (user != null) {
+    if (userPass === user.password) {
+      const token = user.generateAuthToken();
+      user.authTokens.push(token);
+      res.header("x-user-auth-token", token);
+      res.status(200).send();
+    } else res.send("Error");
+  } else res.send("Error");
 });
 module.exports = router;
