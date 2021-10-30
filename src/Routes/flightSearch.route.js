8c1594a1c7ddd80c4c7225e5e5c1e4a0a487ad5c
YousefAH <yousefhmd584@gmail.com>
2021-10-30 21:58:30 +0200

    completed search for flights

--- src/Routes/flightSearch.route.js
+++ src/Routes/flightSearch.route.js
@@ -1,43 +1,42 @@
 const mongoose = require("mongoose");
-//const flightModel = require("../Models/flightModel.js");
+const flightModel = require("../Models/flight.model.js");
 const express = require('express'),
     router = express.Router();
 
 router.get('/',async (req,res)=>{
-    // let flightNumber = req.body.flightNumber;
-    // let departure = req.body.departure;
-    // let arrivalTime = req.body.arrivalTime; 
-    // let dates = req.body.dates;
-    // let economySeats = req.body.economySeats;
-    // let businessSeats = req.body.businessSeats;
-    // let airport = req.body.airport
-    // let queryObj = {};
+    let flightNumber = req.body.flightNumber;
+    let departure = req.body.departure;
+    let arrivalTime = req.body.arrivalTime; 
+    let dates = req.body.dates;
+    let economySeats = req.body.economySeats;
+    let businessSeats = req.body.businessSeats;
+    let airport = req.body.airport
+    let queryObj = {};
 
     
-    // if(flightNumber != '')
-    //     queryObj['flightNumber'] = flightNumber
-    // if(departure != '')
-    //     queryObj['departureTime'] = departure
-    // if(arrivalTime != '')
-    //     queryObj['arrivalTime'] = arrivalTime
-    // if(dates != '')
-    //     queryObj[''] = dates
-    // if(economySeats != '')
-    //     queryObj['economySeats'] = economySeats
-    // if(businessSeats != '')
-    //     queryObj['businessSeats'] = businessSeats
-    // if(airport != '')
-    //     queryObj['airportTerminal.33.'] = airport
+    if(flightNumber != '' && flightNumber != undefined )
+        queryObj['flightNumber'] = flightNumber
+    if(departure != '' && departure != undefined)
+        queryObj['departureTime'] = departure
+    if(arrivalTime != '' && arrivalTime != undefined)
+        queryObj['arrivalTime'] = arrivalTime
+    if(dates != '' && dates != undefined)
+        queryObj[''] = dates
+    if(economySeats != '' && economySeats != undefined)
+        queryObj['economySeats'] = economySeats
+    if(businessSeats != '' && businessSeats != undefined)
+        queryObj['businessSeats'] = businessSeats
+    if(airport != '' && airport != undefined)
+        queryObj['airportTerminal'] = airport
 
-    // const query = flightModel.findOne(queryObj);
-    // query.exec(function (err, flight) {
-    //     if (err) return handleError(err);
+    const query =await flightModel.find(queryObj);
+    
+      console.log(queryObj);
+      console.log(query);
+      res.send(query);
 
-    //     console.log(flight);
-    //     res.send(flight);
-    //   });
-    console.log(req.body);
-    res.status(200).send(req.body);
+
+       
   })
 
 
