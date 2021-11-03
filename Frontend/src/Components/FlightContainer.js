8d42d17d0a74969c332881601761cceb4142ee48
YousefAH <yousefhmd584@gmail.com>
2021-11-04 00:01:43 +0200

    flightContainer test

--- Frontend/src/Components/FlightContainer.js
+++ Frontend/src/Components/FlightContainer.js
@@ -11,14 +11,32 @@ import DraftsIcon from '@mui/icons-material/Drafts';
 import FlightCard from './FlightCard';
 
 export default function FlightContainer() {
+
+  const flight1 = {"flightNumber":1 ,
+  "departureTime": "23-12-2000",
+  "arrivalTime": "23-12-2001" ,
+  "economySeats": 21,
+  "businessSeats": 12,
+  "arrivalTerminal": "CAI",
+  "departureTerminal":"RYD"}
+
+  const flight2 = {"flightNumber":2 ,
+  "departureTime": "23-12-2000",
+  "arrivalTime": "23-12-2001" ,
+  "economySeats": 21,
+  "businessSeats": 12,
+  "arrivalTerminal": "CAI",
+  "departureTerminal":"BUX"}
   return (
-    <Box sx={{ width: '100%', maxWidth: 360,maxHeight: 200, overflow: 'auto', bgcolor: 'background.paper' }}>
+    <Box sx={{ width: '100%', maxWidth: 360,maxHeight: 1000, overflow: 'auto', bgcolor: 'black' }}>
       <nav aria-label="main mailbox folders">
         <List>
           <ListItem disablePadding>
-            <FlightCard flightNumber = '23'/>
+            <FlightCard flight = {flight1}/>
           </ListItem>
-          
+          <ListItem disablePadding>
+            <FlightCard flight = {flight2}/>
+          </ListItem>
         </List>
       </nav>
     </Box>
