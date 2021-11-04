3648089fabb462d0f51d01f10e9c9188305b97ff
Youssef Algendy <ymgendy2000@gmail.com>
2021-11-04 20:14:36 +0200

    Completed login page design

--- Frontend/src/Containers/Login/LoginPage.js
+++ Frontend/src/Containers/Login/LoginPage.js
@@ -7,15 +7,20 @@ import Styles from "./LoginPage.module.css";
 const LoginPage = ({}) => {
   return (
     <div className={Styles.Page}>
-      <p className={Styles.SignInTitle}>Administrator Login</p>
-      <br></br>
-      <FontAwesomeIcon icon={faLock} className={Styles.LockIcon} />
-      <p className={Styles.SignInText}>Sign in</p>
-      <TextBoxDK text="Email Address *" />
-      <br></br>
-      <TextBoxDK text="Password *" />
-      <br></br>
-      <ButtonDK className={Styles.SignInButton} buttonText="Sign in"></ButtonDK>
+      <div className={Styles.PageItems}>
+        <p className={Styles.SignInTitle}>Administrator Login</p>
+        <br></br>
+        <FontAwesomeIcon icon={faLock} className={Styles.LockIcon} />
+        <p className={Styles.SignInText}>Sign in</p>
+        <TextBoxDK text="Email Address *" />
+        <br></br>
+        <TextBoxDK text="Password *" />
+        <br></br>
+        <ButtonDK
+          className={Styles.SignInButton}
+          buttonText="Sign in"
+        ></ButtonDK>
+      </div>
     </div>
   );
 };
