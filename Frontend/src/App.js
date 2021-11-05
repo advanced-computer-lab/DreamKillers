c7b4fb3772ed2b147bd29168d2054843b40cacbe
YousefAH <yousefhmd584@gmail.com>
2021-11-05 18:02:28 +0200

    Created Flight Edit popup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,11 +4,20 @@ import FlightContainer from "./Components/FlightContainer/FlightContainer";
 import FilterDK from "./Components/FilterDK/FilterDK";
 import DropDownDK from "./Components/DropDownDK/DropDownDK";
 import FlightSearchModal from "./Components/FlightSearchModal/FlightSearchModal";
+import FlightEditModal from "./Components/FlightEditModal/FlightEditModal";
 
 function App() {
   return (
     <div className="App">
       <FlightSearchModal
+        mainButtonColor="red"
+        mainButtonText="hi"
+        mainButtonHoverColor="blue"
+        mainButtonTextColor="black"
+        terminals={["CAI", "NYC"]}
+        title="search"
+      />
+      <FlightEditModal
         mainButtonColor="red"
         mainButtonText="hi"
         mainButtonHoverColor="blue"
