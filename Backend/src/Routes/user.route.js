816d4f07fc2d3f70f8be7902dd6f4f1bf4bb163d
Mohammed Hossameldin <falconettolapretso@gmail.com>
2021-11-29 22:41:02 +0200

    Final Review of User Routes

--- Backend/src/Routes/user.route.js
+++ Backend/src/Routes/user.route.js
@@ -19,12 +19,28 @@ router.post("/login", async (req, res) => {
 });
 
 router.patch('/edit', async (req, res) => {
-  const userEmail = req.body.Email;
+  const userEmail = req.body.email;
   const userPass = req.body.password;
+  const userPassportNumber = req.body.passportNumber;
+  const userPhoneNumber = req.body.phoneNumber;
+  const userAge = req.body.userAge;
   const user = await User.findOne({email: userEmail});
+
+  const userDuplicate = await User.find({
+    $or: [
+      { 'email': req.body.email },
+      { 'passportNumber': req.body.passportNumber },
+      { 'phoneNumber': req.body.phoneNumber }
+    ]
+  });
+
+  if (userDuplicate) return res.status(409).send();
 
   user.userEmail = userEmail;
   user.userPass = userPass;
+  user.passportNumber = passportNumber;
+  user.phoneNumber = phoneNumber;
+  user.userAge = userAge;
 
   const response = await user.save();
   res.status(200).send(response);
