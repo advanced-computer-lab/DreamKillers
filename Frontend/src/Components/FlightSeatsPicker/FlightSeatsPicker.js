c8480499df5e621512832e13a96723cf54084f32
Mohammed Hossameldin <falconettolapretso@gmail.com>
2021-12-02 00:09:54 +0200

    Flight Seat Picker Finally Visible

--- Frontend/src/Components/FlightSeatsPicker/FlightSeatsPicker.js
+++ Frontend/src/Components/FlightSeatsPicker/FlightSeatsPicker.js
@@ -7,13 +7,13 @@ export default class App extends Component {
     loading: false
   }
  
-  addSeatCallback = ({ row, number, id }, addCb) => {
+  addSeatCallback = ({ row, number, id}, addCb) => {
     this.setState({
       loading: true
     }, async () => {
       await new Promise(resolve => setTimeout(resolve, 1500))
-      console.log(`Added seat ${number}, row ${row}, id ${id}`)
-      const newTooltip = `tooltip for id-${id} added by callback`
+      // console.log(`Added seat ${number}, row ${row}, id ${id}`)
+      const newTooltip = `Reserved by You`
       addCb(row, number, id, newTooltip)
       this.setState({ loading: false })
     })
@@ -25,12 +25,12 @@ export default class App extends Component {
     }, async () => {
       if (removeCb) {
         await new Promise(resolve => setTimeout(resolve, 750))
-        console.log(`Removed seat ${params.number}, row ${params.row}, id ${params.id}`)
+        // console.log(`Removed seat ${params.number}, row ${params.row}, id ${params.id}`)
         removeCb(params.row, params.number)
       }
       await new Promise(resolve => setTimeout(resolve, 750))
-      console.log(`Added seat ${number}, row ${row}, id ${id}`)
-      const newTooltip = `tooltip for id-${id} added by callback`
+      // console.log(`Added seat ${number}, row ${row}, id ${id}`)
+      const newTooltip = `Reserved by You`
       addCb(row, number, id, newTooltip)
       this.setState({ loading: false })
     })
@@ -41,7 +41,7 @@ export default class App extends Component {
       loading: true
     }, async () => {
       await new Promise(resolve => setTimeout(resolve, 1500))
-      console.log(`Removed seat ${number}, row ${row}, id ${id}`)
+      // console.log(`Removed seat ${number}, row ${row}, id ${id}`)
       // A value of null will reset the tooltip to the original while '' will hide the tooltip
       const newTooltip = ['A', 'B', 'C'].includes(row) ? null : ''
       removeCb(row, number, newTooltip)
@@ -51,11 +51,11 @@ export default class App extends Component {
  
   render() {
     const rows = [
-      [{id: 1, number: 1, isSelected: true, tooltip: 'Reserved by you'}, {id: 2, number: 2, tooltip: 'Cost: 15$'}, null, {id: 3, number: '3', isReserved: true, orientation: 'east', tooltip: 'Reserved by Rogger'}, {id: 4, number: '4', orientation: 'west'}, null, {id: 5, number: 5}, {id: 6, number: 6}],
-      [{id: 7, number: 1, isReserved: true, tooltip: 'Reserved by Matthias Nadler'}, {id: 8, number: 2, isReserved: true}, null, {id: 9, number: '3', isReserved: true, orientation: 'east'}, {id: 10, number: '4', orientation: 'west'}, null, {id: 11, number: 5}, {id: 12, number: 6}],
-      [{id: 13, number: 1}, {id: 14, number: 2}, null, {id: 15, number: 3, isReserved: true, orientation: 'east'}, {id: 16, number: '4', orientation: 'west'}, null, {id: 17, number: 5}, {id: 18, number: 6}],
-      [{id: 19, number: 1, tooltip: 'Cost: 25$'}, {id: 20, number: 2}, null, {id: 21, number: 3, orientation: 'east'}, {id: 22, number: '4', orientation: 'west'}, null, {id: 23, number: 5}, {id: 24, number: 6}],
-      [{id: 25, number: 1, isReserved: true}, {id: 26, number: 2, orientation: 'east'}, null, {id: 27, number: '3', isReserved: true}, {id: 28, number: '4', orientation: 'west'}, null,{id: 29, number: 5, tooltip: 'Cost: 11$'}, {id: 30, number: 6, isReserved: true}]
+      [{id: 1, number: 1, isSelected: true, tooltip: 'Business Class, cost: 20$'}, {id: 2, number: 2, tooltip: 'Business Class, cost: 20$'}, null, {id: 3, number: '3', isReserved: true, orientation: 'east', tooltip: 'Business Class: Reserved by Roger'}, {id: 4, number: '4', orientation: 'west', tooltip: 'Business Class, cost: 20$'}, null, {id: 5, number: 5, tooltip: 'Business Class, cost: 20$'}, {id: 6, number: 6, tooltip: 'Business Class, cost: 20$'}],
+      [{id: 7, number: 1, isReserved: true, tooltip: 'Business Class: Reserved by Mark Antonio'}, {id: 8, number: 2, isReserved: true, tooltip: 'Business Class: Reserved by Mark Antonio'}, null, {id: 9, number: '3', isReserved: true, orientation: 'east', tooltip: 'Business Class: Reserved by Mark Antonio'}, {id: 10, number: '4', orientation: 'west', tooltip: 'Business Class, cost: 20$'}, null, {id: 11, number: 5, tooltip: 'Business Class, cost: 20$'}, {id: 12, number: 6, tooltip: 'Business Class, cost: 20$'}],
+      [{id: 13, number: 1, tooltip: 'Economy Class, cost: 13$'}, {id: 14, number: 2, tooltip: 'Economy Class, cost: 13$'}, null, {id: 15, number: 3, isReserved: true, orientation: 'east', tooltip: 'Economy Class, Reserved by Pamela Watson'}, {id: 16, number: '4', orientation: 'west', tooltip: 'Economy Class, cost: 13$'}, null, {id: 17, number: 5, tooltip: 'Economy Class, cost: 13$'}, {id: 18, number: 6, tooltip: 'Economy Class, cost: 13$'}],
+      [{id: 19, number: 1, tooltip: 'Economy Class, cost: 13$'}, {id: 20, number: 2, tooltip: 'Economy Class, cost: 13$'}, null, {id: 21, number: 3, orientation: 'east', tooltip: 'Economy Class, cost: 13$'}, {id: 22, number: '4', orientation: 'west', tooltip: 'Economy Class, cost: 13$'}, null, {id: 23, number: 5, tooltip: 'Economy Class, cost: 13$'}, {id: 24, number: 6, tooltip: 'Economy Class, cost: 13$'}],
+      [{id: 25, number: 1, isReserved: true, tooltip: 'Economy Class, Reserved by Brenda Lamarr'}, {id: 26, number: 2, orientation: 'east', tooltip: 'Economy Class, cost: 13$'}, null, {id: 27, number: '3', isReserved: true, tooltip: 'Economy Class, Reserved by Brenda Lamarr'}, {id: 28, number: '4', orientation: 'west', tooltip: 'Economy Class, cost: 13$'}, null,{id: 29, number: 5, tooltip: 'Economy Class, cost: 13$'}, {id: 30, number: 6, isReserved: true, tooltip: 'Economy Class, Reserved by Kent Franklin'}]
     ]
     const {loading} = this.state
     return (
@@ -66,7 +66,7 @@ export default class App extends Component {
             addSeatCallback={this.addSeatCallback}
             removeSeatCallback={this.removeSeatCallback}
             rows={rows}
-            maxReservableSeats={3}
+            maxReservableSeats={this.props.seatNumber}
             alpha
             visible
             selectedByDefault
