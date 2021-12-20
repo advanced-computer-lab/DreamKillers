191ed35da8b979a618ac3c48a216a58cfc7286d8
YousefAH <yousefhmd584@gmail.com>
2021-12-20 14:17:48 +0200

    changed style of bookedCard and added new fields

--- Frontend/src/Components/BookedFlightCard/BookedFlightCard.js
+++ Frontend/src/Components/BookedFlightCard/BookedFlightCard.js
@@ -1,65 +1,97 @@
 import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ArrowRightAltOutlinedIcon from "@mui/icons-material/ArrowRightAltOutlined";
-import ButtonDK from "../ButtonDK/ButtonDK";
 import Styles from "./BookedFlightCard.module.css";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-export default function FlightCardTwo({
+export default function BookedFlightCard({
   flight,
+  cabinClass,
   button,
   width,
   icon,
   seats,
   price,
+  seatsButton,
 }) {
+  const getDuration = (date1, date2) => {
+    var difference = Math.abs(new Date(date1) - new Date(date2));
+    let hours = difference / (1000 * 3600);
+    let min = (hours % 1) * 60;
+    hours = Math.floor(hours);
+    return hours + "h " + min + "m";
+  };
+  const getSeats = () => {
+    let res = "";
+    seats.forEach((seat) => {
+      res += seat + ",";
+    });
+    return res.substring(0, res.length - 1);
+  };
+
   return (
-    <div>
-      <div className={Styles.FlightCardContainer}>
-        <Card sx={{ maxWidth: width, borderRadius: 3, border: 1 }}>
-          <CardContent>
-            {icon}
-            <Typography className={Styles.FlightCardDescription}>
-              <div style={{ fontSize: 20 }}>
-                {flight.departureTerminal}
-                <ArrowRightAltOutlinedIcon />
-                {flight.arrivalTerminal}
-              </div>
-              <div>Economy Seats: {flight.economySeats}</div>
-              <div>Bussiness Seats: {flight.businessSeats}</div>
-              <div>
-                Leaves On:{" "}
+    <div className={Styles.FlightCardContainer}>
+      {icon}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Flight Number & Cabin Class</TableCell>
+              <TableCell align="center">Trip Duration</TableCell>
+              <TableCell align="center">Departure Time</TableCell>
+              <TableCell align="center">Arrival Time</TableCell>
+              <TableCell align="center">Baggage Allowance</TableCell>
+              {seats != null ? (
+                <TableCell align="center">Seats</TableCell>
+              ) : null}
+              <TableCell align="center">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+              key={flight.flightNumber}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell align="center" component="th" scope="row">
+                {flight.flightNumber +
+                  (cabinClass == "Bussiness" ? " B" : " E")}
+              </TableCell>
+              <TableCell align="center">
+                {getDuration(flight.arrivalTime, flight.departureTime)}
+              </TableCell>
+              <TableCell align="center">
                 {new Date(flight.departureTime).toDateString() +
                   " " +
                   new Date(flight.departureTime).toLocaleTimeString()}
-              </div>
-              <div>
-                Arrives On:
+              </TableCell>
+              <TableCell align="center">
                 {"\n" +
                   new Date(flight.arrivalTime).toDateString() +
                   " " +
                   new Date(flight.arrivalTime).toLocaleTimeString()}
-              </div>
-              <br></br>
-              {seats == null || price == null ? null : (
-                <div>
-                  {" "}
-                  Seats:{seats.toString()} Price: ${price}
-                </div>
-              )}
-              <div> Baggage Allowanace {" " + flight.baggageAllowance}</div>
-            </Typography>
-          </CardContent>
-          <CardActions>{button}</CardActions>
-        </Card>
+              </TableCell>
+              <TableCell align="center">{flight.baggageAllowance}</TableCell>
+              {seats != null ? (
+                <TableCell align="center">{getSeats()}</TableCell>
+              ) : null}
+
+              <TableCell align="center">{flight.price}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div className={Styles.ButtonsContainer}>
+        {button}
+        <div className={Styles.Button2}>{seatsButton}</div>
       </div>
     </div>
   );
